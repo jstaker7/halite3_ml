450054028a046208181d0065a66ceafaa45c907f
jstaker7 <jstaker7@gmail.com>
2018-10-16 19:56:34 -0700

    Fixed bugs in download script

--- training_module/scripts/download_cron.sh
+++ training_module/scripts/download_cron.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-
-source activate cdeep3
+#!/bin/bash
 
 export PYTHONPATH=/home/staker/Projects/halite/halite3_ml
+
+source activate cdeep3
 
 python /home/staker/Projects/halite/halite3_ml/training_module/scripts/download_replays.py >> /home/staker/logs/download_script.log
 
