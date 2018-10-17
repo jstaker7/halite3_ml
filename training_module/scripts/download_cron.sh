eecae65628ebe80d2713d2ff81f41015d44ff34b
jstaker7 <jstaker7@gmail.com>
2018-10-17 10:37:24 -0700

    Fixed bugs preventing download script from running

--- training_module/scripts/download_cron.sh
+++ training_module/scripts/download_cron.sh
@@ -4,6 +4,6 @@ export PYTHONPATH=/home/staker/Projects/halite/halite3_ml
 
 source activate cdeep3
 
-python /home/staker/Projects/halite/halite3_ml/training_module/scripts/download_replays.py >> /home/staker/logs/download_script.log
+/home/staker/miniconda3/envs/cdeep3/bin/python3.6 /home/staker/Projects/halite/halite3_ml/training_module/scripts/download_replays.py >> /home/staker/logs/download_script.log
 
 source deactivate
--- training_module/scripts/download_replays.py
+++ training_module/scripts/download_replays.py
@@ -1,37 +1,41 @@
-import os
-#import zstd
-#import json
-import datetime
+try:
+    import os
+    #import zstd
+    #import json
+    import datetime
 
-from training_module.hlt_client.download_game import download
+    from training_module.hlt_client.download_game import download
 
-outpath = '/Users/Peace/Desktop/replays'
+    outpath = '/Users/Peace/Desktop/replays'
 
-if not os.path.exists(outpath):
-    outpath = '/home/staker/Projects/halite/replays'
+    if not os.path.exists(outpath):
+        outpath = '/home/staker/Projects/halite/replays'
 
-offset = 2 # To ensure the day is done (timezone differences)
+    offset = 1 # To ensure the day is done (timezone differences)
 
-today = datetime.datetime.now() - datetime.timedelta(days=offset)
-today = str(today).split('-')[:3]
-today[-1] = today[-1].split(' ')[0]
-today = [str(int(x)) for x in today]
+    today = datetime.datetime.now() - datetime.timedelta(days=offset)
+    today = str(today).split('-')[:3]
+    today[-1] = today[-1].split(' ')[0]
+    today = [str(int(x)) for x in today]
 
-date = ''.join(today)
+    date = ''.join(today)
 
-outpath = os.path.join(outpath, date)
+    #date = '20181015'
 
-os.mkdir(outpath)
+    _outpath = os.path.join(outpath, date)
 
-download(outpath, date, False)
+    os.mkdir(_outpath)
 
-print(date)
-print()
+    download(_outpath, date, False)
 
-#for replay_name in os.listdir(outpath):
-#    path = os.path.join(outpath, replay_name)
-#    with open(path, 'rb') as infile:
-#        raw_replay = zstd.loads(infile.read()).decode()
-#        print(raw_replay)
-#        sdfsf
+    print(date)
+    print()
 
+    #for replay_name in os.listdir(outpath):
+    #    path = os.path.join(outpath, replay_name)
+    #    with open(path, 'rb') as infile:
+    #        raw_replay = zstd.loads(infile.read()).decode()
+    #        print(raw_replay)
+    #        sdfsf
+except Exception as e:
+    print(e)
